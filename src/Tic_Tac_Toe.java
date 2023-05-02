dae0d28a19cdefb6bac32db053850faadb577f33
Harshal <harshal19052003@gmail.com>
2023-05-02 19:00:45 +0530

    Added Singleplayer

--- src/Tic_Tac_Toe.java
+++ src/Tic_Tac_Toe.java
@@ -1,6 +1,7 @@
+import java.util.Random;
 import java.util.Scanner;
 public class Tic_Tac_Toe {
-    public static boolean end1(char[][] arr){
+    public static boolean end(char[][] arr){
         if(arr[0][0] == arr[1][1] && arr[1][1] == arr[2][2] && arr[1][1]!='-'){
             return true;
         }else if(arr[2][0] == arr[1][1] && arr[1][1] == arr[0][2] && arr[1][1]!='-'){
@@ -20,42 +21,41 @@ public class Tic_Tac_Toe {
         }
         return false;
     }
-    public static void check(char[][] arr,int q){
-        if(arr[0][0] == arr[1][1] && arr[1][1] == arr[2][2] && arr[1][1]!='-'){
+    public static void check1(char[][] arr, String c) {
+        if (arr[0][0] == arr[1][1] && arr[1][1] == arr[2][2] && arr[1][1] != '-') {
             print(arr);
-            System.out.println("Player " + q +" WON");
+            System.out.println( c + " WON");
             return;
-        }else if(arr[2][0] == arr[1][1] && arr[1][1] == arr[0][2] && arr[1][1]!='-'){
+        } else if (arr[2][0] == arr[1][1] && arr[1][1] == arr[0][2] && arr[1][1] != '-') {
             print(arr);
-            System.out.println("Player "+ q +" WON");
+            System.out.println(c +" WON");
             return;
-        }else if(arr[0][0] == arr[0][1] && arr[0][1] == arr[0][2] && arr[0][1]!='-'){
+        } else if (arr[0][0] == arr[0][1] && arr[0][1] == arr[0][2] && arr[0][1] != '-') {
             print(arr);
-            System.out.println("Player "+ q +" WON");
+            System.out.println(c+" WON");
             return;
-        }else if(arr[1][0] == arr[1][1] && arr[1][1] == arr[1][2] && arr[1][1]!='-'){
+        } else if (arr[1][0] == arr[1][1] && arr[1][1] == arr[1][2] && arr[1][1] != '-') {
             print(arr);
-            System.out.println("Player "+ q +" WON");
+            System.out.println(c +" WON");
             return;
-        }else if(arr[2][0] == arr[2][1] && arr[2][1] == arr[2][2] && arr[2][1]!='-'){
+        } else if (arr[2][0] == arr[2][1] && arr[2][1] == arr[2][2] && arr[2][1] != '-') {
             print(arr);
-            System.out.println("Player "+ q +" WON");
+            System.out.println(c+" WON");
             return;
-        }else if(arr[0][0] == arr[1][0] && arr[1][0] == arr[2][0] && arr[1][0]!='-'){
+        } else if (arr[0][0] == arr[1][0] && arr[1][0] == arr[2][0] && arr[1][0] != '-') {
             print(arr);
-            System.out.println("Player "+ q +" WON");
+            System.out.println(c+" WON");
             return;
-        }else if(arr[0][1] == arr[1][1] && arr[1][1] == arr[2][1] && arr[1][1]!='-'){
+        } else if (arr[0][1] == arr[1][1] && arr[1][1] == arr[2][1] && arr[1][1] != '-') {
             print(arr);
-            System.out.println("Player "+ q +" WON");
+            System.out.println(c+" WON");
             return;
-        }else if(arr[0][2] == arr[1][2] && arr[1][2] == arr[2][2] && arr[1][2]!='-'){
+        } else if (arr[0][2] == arr[1][2] && arr[1][2] == arr[2][2] && arr[1][2] != '-') {
             print(arr);
-            System.out.println("Player "+ q +" WON");
+            System.out.println(c+" WON");
             return;
-        }else{
+        } else {
             print(arr);
-            System.out.println("Next PLayer's Chance");
             return;
         }
     }
@@ -75,68 +75,122 @@ public class Tic_Tac_Toe {
                 arr[i][j]= '-';
             }
         }
-        print(arr);
         Scanner s = new Scanner(System.in);
-//      char p1 = s.next().charAt(0);
-//      char p2 = s.next().charAt(0);
-        System.out.println("Player 1 is o");
-        System.out.println("Player 2 is x");
-        System.out.println("Input Example : row number <space> column number      both rows and columns are from 0 to 2 (ends inclusive) ");
+        System.out.println("Enter mode of play" + "\n" + "for Dumb A.I. : Press 0" + "\n" + "for Multiplayer : Press 1");
+        int nn = s.nextInt();
 
-        for(int i=0;i<9;) {
-            System.out.println("Player 1's Chance");
-            for(int z1=3;z1>0;){
-                int q1 = s.nextInt();
-                int q2 = s.nextInt();
-                if(q1>=0 && q1<3 && q2>=0 && q2<3 && arr[q1][q2]=='-'){
+        if (nn == 0) {
+            print(arr);
+            System.out.println("Input Example : row number <space> column number      both rows and columns are from 0 to 2 (ends inclusive) ");
+            System.out.println("Player : o" +"\n"+"Dumb A.I. : x");
 
-                    arr[q1][q2] = 'o';
-                    check(arr,1);
-                    i++;
-                    if(i>=9){
-                        print(arr);
-                        System.out.println("Game Over No one WON");
+            for (int i = 0; i < 9; ) {
+                System.out.println("Player's Chance");
+                for (int z1 = 3; z1 > 0; ) {
+                    int q1 = s.nextInt();
+                    int q2 = s.nextInt();
+                    if (q1 >= 0 && q1 < 3 && q2 >= 0 && q2 < 3 && arr[q1][q2] == '-') {
+
+                        arr[q1][q2] = 'o';
+                        check1(arr,"Player");
+                        i++;
+                        break;
+                    } else {
+                        z1--;
+                        System.out.println("Invalid Input " + z1 + " moves remaining for Player");
+                    }
+
+                }
+                if (end(arr)) {
+                    break;
+                } else if (i >= 9) {
+                    print(arr);
+                    System.out.println("Game Over No one WON");
+                    break;
+                }
+
+                Random r = new Random();
+
+                for(int j= 0;j<1000;j++){
+                    int n1 = r.nextInt(3);
+                    int n2 = r.nextInt(3);
+                    if (arr[n1][n2] == '-') {
+
+                        arr[n1][n2] = 'x';
+                        System.out.println("Dumb A.I. chose "+ n1 +" "+ n2);
+                        check1(arr, "A.I.");
+                        i++;
                         break;
                     }
+                }if (end(arr)) {
                     break;
-                }else {
-                    z1--;
-                    System.out.println("Invalid Input " + z1 + " moves remaining for Player1");
-                }
-
-            }
-            if(end1(arr)){
-                break;
-            }else if(i==9){
-                print(arr);
-                System.out.println("Game Over No one WON");
-                break;
-            }
-
-            System.out.println("Player 2's Chance");
-
-            for(int z2=3;z2>0;){
-
-                int r1 = s.nextInt();
-                int r2 = s.nextInt();
-                if(r1>=0 && r1<3 && r2>=0 && r2<3 && arr[r1][r2]=='-'){
-
-                    arr[r1][r2] = 'x';
-                    check(arr,2);
-                    i++;
+                } else if (i >= 9) {
+                    print(arr);
+                    System.out.println("Game Over No one WON");
                     break;
-                }else{
-                    z2--;
-                    System.out.println("Invalid Input " + z2 + " moves remaining for Player2");
                 }
             }
-            if(end1(arr)){
-                break;
-            }else if(i==9){
-                print(arr);
-                System.out.println("Game Over No one WON");
-                break;
+        }else if(nn==1){
+            print(arr);
+            System.out.println("Player 1 is o");
+            System.out.println("Player 2 is x");
+            System.out.println("Input Example : row number <space> column number      both rows and columns are from 0 to 2 (ends inclusive) ");
+
+            for(int i=0;i<9;) {
+                System.out.println("Player 1's Chance");
+                for(int z1=3;z1>0;){
+                    int q1 = s.nextInt();
+                    int q2 = s.nextInt();
+                    if(q1>=0 && q1<3 && q2>=0 && q2<3 && arr[q1][q2]=='-'){
+
+                        arr[q1][q2] = 'o';
+                        check1(arr,"Player1");
+                        i++;
+                        if(i>=9){
+                            print(arr);
+                            System.out.println("Game Over No one WON");
+                            break;
+                        }
+                        break;
+                    }else {
+                        z1--;
+                        System.out.println("Invalid Input " + z1 + " moves remaining for Player1");
+                    }
+
+                }
+                if(end(arr)){
+                    break;
+                }else if(i==9){
+                    print(arr);
+                    System.out.println("Game Over No one WON");
+                    break;
+                }
+
+                System.out.println("Player 2's Chance");
+
+                for(int z2=3;z2>0;){
+
+                    int r1 = s.nextInt();
+                    int r2 = s.nextInt();
+                    if(r1>=0 && r1<3 && r2>=0 && r2<3 && arr[r1][r2]=='-'){
+
+                        arr[r1][r2] = 'x';
+                        check1(arr,"Player2");
+                        i++;
+                        break;
+                    }else{
+                        z2--;
+                        System.out.println("Invalid Input " + z2 + " moves remaining for Player2");
+                    }
+                }
+                if(end(arr)){
+                    break;
+                }else if(i==9){
+                    print(arr);
+                    System.out.println("Game Over No one WON");
+                    break;
+                }
             }
         }
-}
+    }
 }
